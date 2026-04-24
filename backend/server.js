3dfae7e8837b9c711b08917f9d23a9c19a15f713
Kanishka Tyagi <krishrk108@gmail.com>
2026-04-24 13:40:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: "https://cc-practical-sigma.vercel.app/"
+  origin: "https://cc-practical-sigma.vercel.app"
 }));
 
 // 🔗 Connect to MongoDB Atlas
